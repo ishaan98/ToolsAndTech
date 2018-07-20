460731549410742ca15adf4ef49f31cb91b40783
Ishaan Banerjee <ishaanb98@gmail.com>
2018-07-20 16:42:09 +0530

    20.07.18

---
+++ p1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Game{
+
+	void show(){
+		System.out.println("playing football");
+	}
+	
+	private class Play{
+	
+		void display(){
+			System.out.println("Mobile Game");
+		}
+	
+	}
+	
+	void showGame(){
+		Play p = new Play();
+		p.display();
+	}
+	
+}
+
+class Cricket{
+
+	public static void main(String args[]){
+		Game g = new Game();
+		g.show();
+		g.showGame();
+	}
+
+}
